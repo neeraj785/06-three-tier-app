d54e2d7b637b4f3ad4c08d4b00e7021a4464e0e0
neeraj <113282202+neeraj785@users.noreply.github.com>
2026-06-20 13:01:16 +0530

    Update EKS cluster version and node group size

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ module "eks" {
     version = "~> 20.0"
 
     cluster_name = "three-tier-cluster"
-    cluster_version = "1.29"
+    cluster_version = "1.3"
     vpc_id = module.vpc.vpc_id
     subnet_ids = module.vpc.private_subnets
 
@@ -28,7 +28,7 @@ module "eks" {
         nodes = {
             min_size = 1
             max_size = 2
-            desired_size = 3
+            desired_size = 1
             instance_types = ["t3.medium"]
         }
     }
