b20211ef954e40c7061e90e6d1dc0b34e4b968d0
neeraj <113282202+neeraj785@users.noreply.github.com>
2026-06-21 14:01:52 +0530

    Remove duplicate cluster endpoint access settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,13 +18,14 @@ module "vpc" {
 module "eks" {
     source = "terraform-aws-modules/eks/aws"
     version = "~> 20.0"
+    cluster_endpoint_public_access  = true
+    cluster_endpoint_private_access = true
 
     cluster_name = "three-tier-cluster"
     cluster_version = "1.32"
     vpc_id = module.vpc.vpc_id
     subnet_ids = module.vpc.private_subnets
-    cluster_endpoint_public_access  = true
-    cluster_endpoint_private_access = true
+   
 
     eks_managed_node_groups = {
         nodes = {
