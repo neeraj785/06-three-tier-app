38d408c136c783687a386fc1d4f6eadf2de5af58
neeraj <113282202+neeraj785@users.noreply.github.com>
2026-06-21 14:12:13 +0530

    Enable cluster creator admin permissions

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,8 @@ module "eks" {
     version = "~> 20.0"
     cluster_endpoint_public_access  = true
     cluster_endpoint_private_access = true
-
+    enable_cluster_creator_admin_permissions = true
+    
     cluster_name = "three-tier-cluster"
     cluster_version = "1.32"
     vpc_id = module.vpc.vpc_id
