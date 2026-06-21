05fe1f9e44d6f3eb53375540c9326debbdeae0cf
neeraj <113282202+neeraj785@users.noreply.github.com>
2026-06-21 13:59:44 +0530

    Enable public and private access for EKS cluster endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,8 @@ module "eks" {
     cluster_version = "1.32"
     vpc_id = module.vpc.vpc_id
     subnet_ids = module.vpc.private_subnets
+    cluster_endpoint_public_access  = true
+    cluster_endpoint_private_access = true
 
     eks_managed_node_groups = {
         nodes = {
